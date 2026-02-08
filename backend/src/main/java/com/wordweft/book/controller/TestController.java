507888e44a9466138b8afcf22a147a6b2791fc87
Srijib <srijibbose@gmail.com>
2026-02-08 16:30:20 +0530

    deployment files

---
+++ backend/src/main/java/com/wordweft/book/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.wordweft.book.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/public")
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "Welcome To WordWeft";
+    }
+}
